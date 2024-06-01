de51fa8d836a1ba84a2e98674b5d15b2c3ab9561
Meunier6969 <123185224+Meunier6969@users.noreply.github.com>
2024-06-01 18:04:26 +0200

    arrow

--- js/retractsidebar.js
+++ js/retractsidebar.js
@@ -3,15 +3,23 @@ let retracted = false;
 function retract() {
     let sideBar = document.getElementById("side-bar-left");
     let arrowColumn = document.getElementById("side-bar-arrow");
+    let arrow = document.querySelector("#side-bar-retractationarrow img");
 
     if (retracted) {
+        // Opening the side bar
         sideBar.style.display = "flex";
         arrowColumn.style.backgroundColor = "coral";
+        arrow.style.transform = "scaleX(-1)";
+        
         retracted = false;
         
     } else {
+        // Closing the side bar
         sideBar.style.display = "none";
         arrowColumn.style.backgroundColor = "rgba(0,0,0,0)";
+        
+        arrow.style.transform = "scaleX(1)";
         retracted = true;
     }
+
 }
