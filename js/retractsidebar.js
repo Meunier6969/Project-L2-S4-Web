1c4001f1459c3b00bb95053c491056e986651af7
Meunier6969 <123185224+Meunier6969@users.noreply.github.com>
2024-06-01 18:13:32 +0200

    border

--- js/retractsidebar.js
+++ js/retractsidebar.js
@@ -1,9 +1,13 @@
+let sideBar = document.getElementById("side-bar-left");
+let iconsColumn = document.getElementById("side-bar-icons");
+let arrowColumn = document.getElementById("side-bar-arrow");
+let arrow = document.querySelector("#side-bar-retractationarrow img");
+
+// Opened by default
 let retracted = false;
+iconsColumn.style.borderRight = "0pt solid";
 
 function retract() {
-    let sideBar = document.getElementById("side-bar-left");
-    let arrowColumn = document.getElementById("side-bar-arrow");
-    let arrow = document.querySelector("#side-bar-retractationarrow img");
 
     if (retracted) {
         // Opening the side bar
@@ -11,14 +15,20 @@ function retract() {
         arrowColumn.style.backgroundColor = "coral";
         arrow.style.transform = "scaleX(1)";
         
+        arrowColumn.style.borderRight = "3pt solid";
+        iconsColumn.style.borderRight = "0pt solid";
+        
         retracted = false;
         
     } else {
         // Closing the side bar
         sideBar.style.display = "none";
         arrowColumn.style.backgroundColor = "rgba(0,0,0,0)";
-        
         arrow.style.transform = "scaleX(-1)";
+        
+        arrowColumn.style.borderRight = "0pt solid";
+        iconsColumn.style.borderRight = "3pt solid";
+
         retracted = true;
     }
 
