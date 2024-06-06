b75e90aed699196971b3eaba521aece78d69d5d9
Natis-san <62031815+LeBN@users.noreply.github.com>
2024-06-06 09:01:36 +0200

    pp

--- js/retractsidebar.js
+++ js/retractsidebar.js
@@ -5,6 +5,7 @@ let arrow = document.querySelector("#side-bar-retractationarrow img");
 
 // Opened by default
 let retracted = false;
+let canBeOpened = true;
 iconsColumn.style.borderRight = "0pt solid";
 
 function toggleSideBar() {
@@ -29,29 +30,38 @@ function retractSideBar() {
 }
 
 function openSideBar() {
-    sideBar.style.display = "flex";
-    // arrowColumn.style.backgroundColor = "coral";
-    arrow.style.transform = "scaleX(1)";
+    if(canBeOpened) {
+        sideBar.style.display = "flex";
+        // arrowColumn.style.backgroundColor = "coral";
+        arrow.style.transform = "scaleX(1)";
 
-    arrowColumn.style.borderRight = "3pt solid var(--grey)";
-    iconsColumn.style.borderRight = "0pt solid";
-    iconsColumn.style.marginLeft = "2.5em";
-    iconsColumn.style.paddingRight = "0.7em";
-    iconsColumn.style.marginRight = "0em";
+        arrowColumn.style.borderRight = "3pt solid var(--grey)";
+        iconsColumn.style.borderRight = "0pt solid";
+        iconsColumn.style.marginLeft = "2.5em";
+        iconsColumn.style.paddingRight = "0.7em";
+        iconsColumn.style.marginRight = "0em";
 
-    retracted = false;
+        retracted = false;
+    }
+}
+
+function lowerMain() {
+    document.getElementById("categories").style.marginTop = "";
 }
 
 function verifyIfRectractation() {
     if(window.innerWidth < 1565){
+        canBeOpened = false;
         retractSideBar();
     }
     else if (window.innerWidth > 1565) {
+        canBeOpened = true;
         openSideBar();
     }
     if(window.innerWidth < 500){
-            
+        lowerMain();
     }
 }
 
 window.addEventListener('resize', verifyIfRectractation);
+window.addEventListener('load', verifyIfRectractation);
