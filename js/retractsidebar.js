c1ab0d5f238f7a6a17e048f2a2e2bcd026b117d5
Meunier6969 <123185224+Meunier6969@users.noreply.github.com>
2024-06-11 20:22:13 +0200

    transitions

--- js/retractsidebar.js
+++ js/retractsidebar.js
@@ -6,43 +6,12 @@ let arrow = document.querySelector("#side-bar-retractationarrow img");
 // Opened by default
 let retracted = false;
 let canBeOpened = true;
-iconsColumn.style.borderRight = "0pt solid";
+// iconsColumn.style.borderRight = "0pt solid";
 
 function toggleSideBar() {
-    if (retracted) 
-        openSideBar();
-    else 
-        retractSideBar();
-}
-
-function retractSideBar() {
-    sideBar.style.display = "none";
-    arrowColumn.style.backgroundColor = "rgba(0,0,0,0)";
-    arrow.style.transform = "scaleX(-1)";
-    
-    arrowColumn.style.borderRight = "0pt solid";
-    iconsColumn.style.borderRight = "3pt solid var(--grey)";
-    iconsColumn.style.marginLeft = "1em";
-    iconsColumn.style.paddingRight = "1em";
-    iconsColumn.style.marginRight = "0.5em";
-
-    retracted = true;
-}
-
-function openSideBar() {
-    if(canBeOpened) {
-        sideBar.style.display = "flex";
-        // arrowColumn.style.backgroundColor = "coral";
-        arrow.style.transform = "scaleX(1)";
-
-        arrowColumn.style.borderRight = "3pt solid var(--grey)";
-        iconsColumn.style.borderRight = "0pt solid";
-        iconsColumn.style.marginLeft = "2.5em";
-        iconsColumn.style.paddingRight = "0.7em";
-        iconsColumn.style.marginRight = "0em";
-
-        retracted = false;
-    }
+    // if (retracted) openSideBar();
+    // else retractSideBar();
+    document.getElementById("side-bar").classList.toggle("side-bar-closed")
 }
 
 function lowerMain() {
@@ -53,38 +22,26 @@ function upperMain() {
     document.getElementById("categories").style.marginTop = "1em";
 }
 
-function headerBottom() {
+function headerBottom() {}
 
-}
-
-function headerSide() {
-    
-}
+function headerSide() {}
 
 function verifyIfRectractation() {
-    if(window.innerWidth < 1565){
-        canBeOpened = false;
-        retractSideBar();
+    if (window.innerWidth < 1565) {
         document.getElementById("side-bar-arrow").style.display = "none";
-    }
-    else if (window.innerWidth > 1565) {
-        canBeOpened = true;
-        openSideBar();
+        document.getElementById("side-bar").classList.add("side-bar-closed");
+    } else if (window.innerWidth > 1565) {
         document.getElementById("side-bar-arrow").style.display = "flex";
-        document.getElementById("side-bar-arrow").style.justifyContent = "center";
     }
-    if (window.innerWidth < 1300){
+
+    if (window.innerWidth < 1300) {
         lowerMain();
-    }
-    else if (window.innerWidth > 1300) {
+    } else if (window.innerWidth > 1300) {
         upperMain();
     }
     if (window.innerWidth < 983) {
-        
-    }
-    else if (window.innerWidth > 983) {
-        
+    } else if (window.innerWidth > 983) {
     }
 }
 
-window.addEventListener('resize', verifyIfRectractation);
+window.addEventListener("resize", verifyIfRectractation);
